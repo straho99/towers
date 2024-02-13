7924c46cee3be2f339bc4a333f5adc5a6da65be6
StrahilRuychev <strahil.ruychev@gmail.com>
2024-02-13 19:02:04 +0200

    Initial solution

--- towers/main.cpp
+++ towers/main.cpp
@@ -1,14 +1,16 @@
-//
-//  main.cpp
-//  towers
-//
-//  Created by Strahil Ruychev on 13.02.24.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int redHeight, blueHeight, redBoxes, blueBoxes;
+    
+    cin >> redHeight >> blueHeight >> redBoxes >> blueBoxes;
+    
+    int smallerCount = min(redBoxes, blueBoxes);
+    
+    int combinations = (smallerCount * smallerCount) / 2 + (smallerCount * smallerCount) + 1;
+    
+    cout << combinations << endl;
+    
     return 0;
 }
